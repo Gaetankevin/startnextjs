33c38f92856bb3e39626ad9ef4b141245b25490b
gaetan <g45398364@gmail.com>
2025-10-21 00:04:34 +0100

    Ajout du projet Next.js complet

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+//importans tailwind la config globale dans le layout
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body >{children}</body>
+    </html>
+  );
+}
